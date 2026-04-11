f7c548367913060a783d177d1a17b92e187d77fa
rahilaode <laoderahilatansa@gmail.com>
2026-04-12 02:07:38 +0700

    add command to run schema monitor &  register connector

--- run.sh
+++ run.sh
@@ -12,3 +12,20 @@ docker-compose -f ./setup/data-federation/docker-compose.yaml up --build --detac
 # vkg-system
 docker-compose -f ./setup/vkg-system/docker-compose.yaml down -v
 docker-compose -f ./setup/vkg-system/docker-compose.yaml up --build --detach
+
+# ascam schema monitor
+docker-compose -f ./setup/ascam/schema-monitor/docker-compose.yaml down -v
+docker-compose -f ./setup/ascam/schema-monitor/docker-compose.yaml up --build --detach
+
+sleep 40
+
+# register postgres connector with ascam schema monitor
+curl -X POST -H "Content-Type: application/json" \
+     --data @./setup/ascam/schema-monitor/register-postgres.json \
+     http://localhost:8083/connectors
+
+# register mysql connector with ascam schema monitor
+curl -X POST -H "Content-Type: application/json" \
+     --data @./setup/ascam/schema-monitor/register-mysql.json \
+     http://localhost:8083/connectors
+
